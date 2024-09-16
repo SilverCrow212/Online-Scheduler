7f5c35250d6044b251e32a00be1db7578de8b971
SilverCrow212 <dixonlexther21@gmail.com>
2024-09-16 21:15:11 +0800

    update for backend

---
+++ backend/app/Http/Controllers/OnlineSchedulerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OnlineSchedulerController extends Controller
+{
+    public function Dashboard(){
+
+    }
+
+    public function CreateAccount(){
+
+    }
+
+    public function SaveAppointment(){
+
+
+    }
+
+    public function StoreLogs(){
+
+    }
+}
