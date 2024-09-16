fb75519ca566e310317797c3929eb8f3c6f71322
SilverCrow212 <dixonlexther21@gmail.com>
2024-09-16 22:01:21 +0800

    updated backend for login

--- frontendSched/src/views/LoginPage/Login.vue
+++ frontendSched/src/views/LoginPage/Login.vue
@@ -4,7 +4,11 @@ import { ref, computed } from 'vue';
 import AppConfig from '@/layout/AppConfig.vue';
 
 const { layoutConfig } = useLayout();
-const email = ref('');
+const login = ref({
+    idnum : null,
+    password: null
+})
+const idnum = ref('');
 const password = ref('');
 const checked = ref(false);
 
@@ -24,11 +28,11 @@ const logoUrl = computed(() => {
                     </div>
 
                     <div>
-                        <label for="idnumber" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="idnumber" type="text" placeholder="ID number" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="email" />
+                        <label for="idnumber" class="block text-900 text-xl font-medium mb-2">ID number</label>
+                        <InputText id="idnumber" type="text" placeholder="ID number" class="w-full md:w-30rem mb-5" style="padding: 1rem" v-model="login.idnum" />
 
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" :inputStyle="{ padding: '1rem' }"></Password>
+                        <Password id="password1" v-model="login.password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" :inputStyle="{ padding: '1rem' }"></Password>
 
                         <div class="flex align-items-center justify-content-between mb-5 gap-5">
                             <div class="flex align-items-center">
