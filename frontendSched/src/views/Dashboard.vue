a2aa01dad0132ad8ff8e4665561458368ed134e4
SilverCrow212 <dixonlexther21@gmail.com>
2024-09-20 23:18:42 +0800

    update for records and limiting schedule

--- frontendSched/src/views/Dashboard.vue
+++ frontendSched/src/views/Dashboard.vue
@@ -1,51 +1,14 @@
 <script setup>
-import { ref,computed } from 'vue';
+import { ref,computed,watch } from 'vue';
 import axios from 'axios';
 
-const fetchDashboardData = async () => {
-  try {
-    const response = await axios.get('/api/all-patients');
-    dashboardData.value = response.data.patient_details;
-    dashboardToday.value= response.data.total_patients_today;
-    dashboardTotal.value=response.data.total_patients;
-    // console.log('Today',dashboardToday.value);
-  } catch (error) {
-    console.error('Error fetching all patient data:', error);
-  }
-};
-
-const calendarValue = ref(null);
-
-const selectButtonValue1 = ref(null);
-const selectButtonValues1 = ref([
-  { name: '8:00 am - 9:00 am', disabled: false },
-  { name: '9:00 am - 10:00 am', disabled: false },
-  { name: '10:00 am - 11:00 am', disabled: true }, // Example of a disabled option
-  { name: '11:00 am - 12:00 pm', disabled: false },
-  { name: '1:00 pm - 2:00 pm', disabled: false },
-  { name: '2:00 pm - 3:00 pm', disabled: false },
-  { name: '3:00 pm - 4:00 pm', disabled: false },
-  { name: '4:00 pm - 5:00 pm', disabled: false },
-]);
-
-// Selected value
-
-// Function to determine if an option should be disabled
-const isOptionDisabled = (option) => {
-  return option.disabled;
-};
-// const minDate= new Date();
 
 const filledSlots = ref(['2024-05-28', '2024-05-30']);
 
-// Compute min and max dates based on your business logic
+
 const minDate = new Date();
 
 
-// Function to disable filled slots
-// const disabledDates = computed(() => {
-//   return filledSlots.value.map(date => new Date(date));
-// });
 
 
 // Selected date
@@ -63,10 +26,6 @@ const checkboxOptions = ref([
 ]);
 // { label: '4:00 pm - 5:00 pm', value: 8, checked: false },
 
-// Function to check if an option is disabled
-const isOptionDisableds = (option) => {
-  return returnedData.value.includes(option.label);
-};
 
 
 const formatDate = (date) => {
@@ -77,18 +36,11 @@ const formatDate = (date) => {
 };
 const selectedCategories=ref();
 
-const categories = ref([
-    { name: 'Accounting', key: 'A' },
-    { name: 'Marketing', key: 'M' },
-    { name: 'Production', key: 'P' },
-    { name: 'Research', key: 'R' }
-]);
-
 const selectedCategory = ref(null);
 
 const sampleDates = ref(
   [
-    {date: '09/20/2024' , available_time: [
+    {date: '09/23/2024' , available_time: [
       '8:00 am - 9:00 am',
       '9:00 am - 10:00 am', 
       '10:00 am - 11:00 am',
@@ -98,7 +50,7 @@ const sampleDates = ref(
       '3:00 pm - 4:00 pm',
       ]
     },
-    {date: '09/21/2024' , available_time: [
+    {date: '09/25/2024' , available_time: [
       '9:00 am - 10:00 am', 
       '10:00 am - 11:00 am',
       '11:00 am - 12:00 pm',
@@ -138,93 +90,47 @@ const isCategoryDisabled = (category) => {
   // If the date exists, check if the category (time slot) is included in available_time
   return dateInfo ? dateInfo.available_time.includes(category) : false;
 };
+
+watch(selectedDate, () => {
+  selectedCategory.value = null;
+});
+
 </script>
 
 <template>
       <div class="grid p-fluid">
         <div class="col-12 md:col-12">
             <div class="card">
-                <!-- <h5>Icons</h5>
-                <div class="grid formgrid">
-                    <div class="col-12 mb-2 lg:col-6 lg:mb-0">
-                        <IconField>
-                            <InputIcon class="pi pi-user" />
-                            <InputText type="text" placeholder="Username" />
-                        </IconField>
-                    </div>
-                    <div class="col-12 mb-2 lg:col-6 lg:mb-0">
-                        <IconField iconPosition="left">
-                            <InputText type="text" placeholder="Search" />
-                            <InputIcon class="pi pi-search" />
-                        </IconField>
-                    </div>
-                </div> -->
+              <div class="field col-12 md:col-12">
+                  <label>ID number</label>
+                  <InputText id="firstname2" type="text" />
+              </div>
 
-                <h5>Calendar</h5>
-                <Calendar :showIcon="true" :showButtonBar="true" v-model="calendarValue" :minDate="minDate"></Calendar>
-                <h5>SelectButton</h5>
-                <SelectButton
-                class="p-select-button"
-                v-model="selectButtonValue1"
-                :options="selectButtonValues1"
-                optionLabel="name"
-                :disabled="isOptionDisabled"
-                />
-                
-
+              <div class="field col-12 md:col-12">
+                <label>Choose Appointment Date</label>
                 <Calendar
-                :showIcon="true" 
-                :showButtonBar="true"
-                v-model="selectedDate"
-                :min-date="minDate"
-                :disabled-dates="disabledDates"
+                  :showIcon="true" 
+                  :showButtonBar="true"
+                  v-model="selectedDate"
+                  :min-date="minDate"
+                  :disabled-dates="disabledDates"
                 />
-                <!-- <Checkbox
-                v-for="option in checkboxOptions"
-                :key="option.label"
-                v-model="option.checked"
-                :label="option.label"
-                :value="option.value"
-                :disabled="isOptionDisabled(option)"
-                >
-                {{ option.label }}
-                
-            </Checkbox> -->
+              </div>
+              <div class="field col-12 md:col-12"  v-if="selectedDate!==null">
+                <label>Choose Time</label>
+                <div v-for="category in checkboxOptions" class="flex align-items-center" :key="category">
+                  <RadioButton 
+                    v-model="selectedCategory" 
+                    name="dynamic" 
+                    :value="category" 
+                    :disabled="isCategoryDisabled(category)" 
+                  />
+                  <label :for="category" class="ml-2">{{ category }}</label>
+                </div>
+              </div>
             
-            <p v-if="selectedDate!=null">Selected Date: {{ formatDate(selectedDate) }}</p>
-
-            <!-- {{ selectedDate }} -->
-            <!-- <div v-for="category of checkboxOptions" :key="category.key" class="flex align-items-center">
-                <Checkbox v-model="selectedCategories" :inputId="category.key" name="category" :value="category.label" />
-                <label :for="category.key">{{ category.label }}</label>
-            </div> -->
-            {{ selectedCategories }}
-            </div>
-            <!-- <div v-for="category in categories" :key="category.key" class="flex items-center">
-                <RadioButton v-model="selectedCategory" :inputId="category.key" name="dynamic" :value="category.label" />
-                <label :for="category.key" class="ml-2">{{ category.name }}</label>
-            </div> -->
-
-            <!-- <div v-for="category in checkboxOptions"  class="flex align-items-center">
-                <RadioButton v-model="selectedCategory" name="dynamic" :value="category" />
-                <label :for="category" class="ml-2">{{ category }}</label>
-            </div> -->
-
-            <Calendar
-              :showIcon="true" 
-              :showButtonBar="true"
-              v-model="selectedDate"
-              :min-date="minDate"
-              :disabled-dates="disabledDates"
-            />
-            <div v-for="category in checkboxOptions" class="flex align-items-center" :key="category">
-              <RadioButton 
-                v-model="selectedCategory" 
-                name="dynamic" 
-                :value="category" 
-                :disabled="isCategoryDisabled(category)" 
-              />
-              <label :for="category" class="ml-2">{{ category }}</label>
+              <!-- <p v-if="selectedDate!=null">Selected Date: {{ formatDate(selectedDate) }}</p> -->
+              <!-- {{ selectedCategories }} -->
             </div>
             <Button label="Submit" />
         </div>
--- frontendSched/src/views/PatientDetails/PatientRecords.vue
+++ frontendSched/src/views/PatientDetails/PatientRecords.vue
@@ -10,14 +10,21 @@ const products = ref();
 const selectedProduct = ref();
 const metaKey = ref(true);
 
+const patients = ref(
+    [{id:'1', name:'Doe, John Jr.', type:'Student', department:'College of Agriculture'},
+    {id:'2', name:'Curtis, Anne', type:'Faculty', department:'College of Information Sciences'},
+    {id:'3', name:'Reyes, Raul', type:'Staff', department:'Office of Student Affairs'}
+
+    ]
+);
 </script>
 
 <template>
     <div class="grid">
         <div class="col-12">
             <div class="card">
-                <!-- :value="products" -->
-                <DataTable v-model:selection="selectedProduct"  selectionMode="multiple" :metaKeySelection="metaKey" dataKey="id" tableStyle="min-width: 50rem">
+                
+                <DataTable v-model:selection="selectedProduct" :value="patients" selectionMode="single" :metaKeySelection="metaKey" dataKey="id" tableStyle="min-width: 50rem">
                     <Column field="name" header="Patient Name"></Column>
                     <Column field="type" header="Type"></Column>
                     <Column field="department" header="Department"></Column>
