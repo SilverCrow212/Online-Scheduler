d4e897c4b9571886c99c4f64295822d1bab4482b
SilverCrow212 <dixonlexther21@gmail.com>
2024-09-24 21:52:37 +0800

    update for patient records pop up

--- frontendSched/src/views/PatientDetails/PatientRecords.vue
+++ frontendSched/src/views/PatientDetails/PatientRecords.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import { ProductService } from '@/service/ProductService';
+import PatientRecordTable from '@/views/PatientDetails/PatientRecordTable.vue'
+// import { ProductService } from '@/service/ProductService';
 
 // onMounted(() => {
 //     ProductService.getProductsMini().then((data) => (products.value = data));
 // });
 
 const products = ref();
-const selectedProduct = ref();
+const selectedPatient = ref();
 const metaKey = ref(true);
 
+const visible = ref(false);
 const patients = ref(
     [{id:'1', name:'Doe, John Jr.', type:'Student', department:'College of Agriculture'},
     {id:'2', name:'Curtis, Anne', type:'Faculty', department:'College of Information Sciences'},
@@ -17,6 +19,11 @@ const patients = ref(
 
     ]
 );
+
+function dialogOpen(){
+    visible.value = true;
+
+}
 </script>
 
 <template>
@@ -24,7 +31,7 @@ const patients = ref(
         <div class="col-12">
             <div class="card">
                 
-                <DataTable v-model:selection="selectedProduct" :value="patients" selectionMode="single" :metaKeySelection="metaKey" dataKey="id" tableStyle="min-width: 50rem">
+                <DataTable v-model:selection="selectedPatient" :value="patients" selectionMode="single" :metaKeySelection="metaKey" dataKey="id" tableStyle="min-width: 50rem" @row-click="dialogOpen()">
                     <Column field="name" header="Patient Name"></Column>
                     <Column field="type" header="Type"></Column>
                     <Column field="department" header="Department"></Column>
@@ -32,6 +39,24 @@ const patients = ref(
             </div>
         </div>
     </div>
+
+
+    <Dialog v-model:visible="visible" modal :header="selectedPatient?.name" :style="{ width: '70rem' }">
+            <!-- <span class="p-text-secondary block mb-5">Update your information.</span> -->
+            <!-- <div class="flex align-items-center gap-3 mb-3">
+                <label for="username" class="font-semibold w-6rem">Username</label>
+                <InputText id="username" class="flex-auto" v-model="selectedPatient.id" autocomplete="off" />
+            </div>
+            <div class="flex align-items-center gap-3 mb-5">
+                <label for="email" class="font-semibold w-6rem">Email</label>
+                <InputText id="email" class="flex-auto" autocomplete="off" />
+            </div> -->
+            <PatientRecordTable/>
+            <div class="flex justify-content-end gap-2">
+                <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+                <Button type="button" label="Save" @click="visible = false"></Button>
+            </div>
+        </Dialog>
 </template>
 
 <style scoped lang="scss">
@@ -43,3 +68,4 @@ const patients = ref(
     font-weight: bold;
 }
 </style>
+
