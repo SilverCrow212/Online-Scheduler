032b37b9bf881c991cf8483c615b542459d97995
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-09-29 10:35:04 +0800

    updated UI for sidebar

--- frontendSched/src/layout/AppMenu.vue
+++ frontendSched/src/layout/AppMenu.vue
@@ -5,35 +5,38 @@ import AppMenuItem from './AppMenuItem.vue';
 
 const model = ref([
     {
-        label: 'Dashboard',
+        label: 'Menu',
         items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
-        { label: 'Appointments', icon: 'pi pi-fw pi-home', to: '/' }],
+        { label: 'Appointments', icon: 'pi pi-calendar-plus', to: '/appointment' },
+        { label: 'Create Account', icon: 'pi pi-user-plus', to: '/create-account' },
+        { label: 'Patient Records', icon: 'pi pi-folder-open', to: '/patient-records' }
+        ],
         
     },
-    {
-        label: 'Appointments',
-        items: [{ label: 'Appointments', icon: 'pi pi-fw pi-home', to: '/appointment' }]
-    },
-    {
-        label: 'Patient List',
-        items: [{ label: 'Patient List', icon: 'pi pi-fw pi-home', to: '/' }]
-    },
-    {
-        label: 'Reports',
-        items: [{ label: 'Reports', icon: 'pi pi-fw pi-home', to: '/' }]
-    },
-    {
-        label: 'Logs',
-        items: [{ label: 'Logs', icon: 'pi pi-fw pi-home', to: '/' }]
-    },
-    {
-        label: 'Account Creation',
-        items: [{ label: 'Create Account', icon: 'pi pi-fw pi-home', to: '/create-account' }]
-    },
-    {
-        label: 'Patient Records',
-        items: [{ label: 'Patient Records', icon: 'pi pi-fw pi-home', to: '/patient-records' }]
-    },
+    // {
+    //     label: 'Appointments',
+    //     items: [{ label: 'Appointments', icon: 'pi pi-fw pi-home', to: '/appointment' }]
+    // },
+    // {
+    //     label: 'Patient List',
+    //     items: [{ label: 'Patient List', icon: 'pi pi-fw pi-home', to: '/' }]
+    // },
+    // {
+    //     label: 'Reports',
+    //     items: [{ label: 'Reports', icon: 'pi pi-fw pi-home', to: '/' }]
+    // },
+    // {
+    //     label: 'Logs',
+    //     items: [{ label: 'Logs', icon: 'pi pi-fw pi-home', to: '/' }]
+    // },
+    // {
+    //     label: 'Account Creation',
+    //     items: [{ label: 'Create Account', icon: 'pi pi-fw pi-home', to: '/create-account' }]
+    // },
+    // {
+    //     label: 'Patient Records',
+    //     items: [{ label: 'Patient Records', icon: 'pi pi-fw pi-home', to: '/patient-records' }]
+    // },
 ]);
 </script>
 
