5d21c9888192f10506f87c5dbc554d1550a181d5
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-09-30 18:15:44 +0800

    sample update for records final page 3
    ask if can be editable or not

--- frontendSched/src/views/PatientDetails/Record.vue
+++ frontendSched/src/views/PatientDetails/Record.vue
@@ -27,7 +27,7 @@ import PageTwo from '@/views/PatientDetails/PageTwo.vue';
             <StepperPanel header="Step III">
                 <template #content="{ prevCallback }">
                     <div class="flex flex-column h-12rem">
-                        <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content III</div>
+                        <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Review before saving? Add Mark as done button here</div>
                     </div>
                     <div class="flex pt-4 justify-content-between">
                         <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
