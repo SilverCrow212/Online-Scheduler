b4cdd7b6fce78c88bb498ad011ea8b475ebef31a
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-09-30 16:41:55 +0800

    mini update for page one
    did not add v-model for each selections

--- frontendSched/src/views/PatientDetails/PageOne.vue
+++ frontendSched/src/views/PatientDetails/PageOne.vue
@@ -16,16 +16,37 @@ const categories = ref([
 ]);
 const categories2 = ref([
     {name: "Crowding", key: "B", id:1},
-    {name: "Spacing", key: "B", id:1},
-    {name: "Overjet", key: "B", id:1},
-    {name: "Overbite", key: "B", id:1},
-    {name: "Open bite", key: "B", id:1},
-    {name: "Underbite", key: "B", id:1},
-    {name: "Crossbite", key: "B", id:1},
-    {name: "Midline Deviation", key: "B", id:1},
-    {name: "Abnormal eruption", key: "B", id:1},
-    {name: "Class (Molar)", key: "B", id:1},
+    {name: "Spacing", key: "B", id:2},
+    {name: "Overjet", key: "B", id:3},
+    {name: "Overbite", key: "B", id:4},
+    {name: "Open bite", key: "B", id:5},
+    {name: "Underbite", key: "B", id:6},
+    {name: "Crossbite", key: "B", id:7},
+    {name: "Midline Deviation", key: "B", id:8},
+    {name: "Abnormal eruption", key: "B", id:9},
+    {name: "Class (Molar)", key: "B", id:10},
 
+]);
+const categories3 = ref([
+    {name: "Braces", key: "B", id:1},
+    {name: "Retainer", key: "B", id:2},
+    {name: "Other Ortho", key: "B", id:3},
+    {name: "Crown", key: "B", id:4},
+    {name: "Bridge", key: "B", id:5},
+    {name: "Implant", key: "B", id:6},
+    {name: "CD", key: "B", id:7},
+    {name: "RPD", key: "B" , remarks:null, id:8},
+]);
+
+const categories4 = ref([
+    {name: "Pain in the:", key: "B", id:1},
+    {name: "Jaw", key: "B", id:2},
+    {name: "Temple", key: "B", id:3},
+    {name: "Ear", key: "B", id:4},
+    {name: "Clicking", key: "B", id:5},
+    {name: "Locking", key: "B", id:6},
+    {name: "Others", key: "B", id:7},
+    
 ]);
 const selectedCategories = ref(['Bleeding']);
 </script>
@@ -49,6 +70,48 @@ const selectedCategories = ref(['Bleeding']);
             <div v-for="category of categories2" :key="category.key" class="col-6" >
                 <Checkbox v-model="selectedCategories" :inputId="category.key" name="category" :value="category.name" />
                 <label :for="category.key">{{ category.name }}</label>
+            </div>
+        </div>
+        <h5>Appliances</h5>
+        <div class="grid formgrid col-12 p-0 m-0">
+            <div v-for="category of categories3" :key="category.key" class="col-6" >
+                <Checkbox v-model="selectedCategories" :inputId="category.key" name="category" :value="category.name" />
+                <label :for="category.key">{{ category.name }}</label>
+                <div v-if="category.id ===8">
+                    <InputText type="text" class="w-full"/>
+                </div>
+            </div>
+        </div>
+        <h5>TMD</h5>
+        <div class="grid formgrid col-12 p-0 m-0">
+            <div v-for="category of categories4" :key="category.key" class="col-6" >
+                <Checkbox v-model="selectedCategories" :inputId="category.key" name="category" :value="category.name" />
+                <label :for="category.key">{{ category.name }}</label>
+            </div>
+        </div>
+        <h5>Others</h5>
+        <div class="col-12 p-0 m-0">
+            <div class="field col-12 md:col-12">
+                <InputText type="text" class="w-full"/>
+            </div>
+        </div>
+        <h5>Date of Examination</h5>
+        <div class="grid formgrid col-12 p-0 m-0" >
+            <div class="field col-3 md:col-3" >
+                <InputText type="text" class="w-full"/>
+                <label>&nbsp;&nbsp;E G F P /</label>
+            </div>
+            <div class="field col-3 md:col-3">
+                <InputText type="text" class="w-full"/>
+                <label>&nbsp;&nbsp;E G F P /</label>
+            </div>
+            <div class="field col-3 md:col-3">
+                <InputText type="text" class="w-full"/>
+                <label>&nbsp;&nbsp;E G F P /</label>
+            </div>
+            <div class="field col-3 md:col-3">
+                <InputText type="text" class="w-full"/>
+                <label>&nbsp;&nbsp;E G F P </label>
             </div>
         </div>
     </div>
