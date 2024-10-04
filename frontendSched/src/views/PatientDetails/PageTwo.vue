ec362fd2d586123ff47ec334249d4b5c66bc5f7c
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-05 02:06:16 +0800

    added the multiselect feature

--- frontendSched/src/views/PatientDetails/PageTwo.vue
+++ frontendSched/src/views/PatientDetails/PageTwo.vue
@@ -1,25 +1,45 @@
 <script setup>
 import {ref} from 'vue'
+import {serviceRendered} from '@/store/choices'
+const serviceRenderedStore = serviceRendered();
+const serviceChoices = serviceRenderedStore.legend;
 
+const multiselectValue = ref(null);
 const value = ref();
 </script>
 <template>
-    <h5>Remarks</h5>
+    <h5>Services Rendered</h5>
+    <MultiSelect v-model="multiselectValue" :options="serviceChoices" optionLabel="name" placeholder="Select Services" :filter="true" class="w-full">
+        <template #value="slotProps">
+            <div class="inline-flex align-items-center py-1 px-2 bg-primary text-primary border-round mr-2" v-for="option in slotProps.value" :key="option.id">
+                <div>{{ option.name }}</div>
+            </div>
+            <template v-if="!slotProps.value || slotProps.value.length === 0">
+                <div class="p-1">Select Services</div>
+            </template>
+        </template>
+        <template #option="slotProps">
+            <div class="flex align-items-center">
+                <div>{{ slotProps.option.name }}</div>
+            </div>
+        </template>
+    </MultiSelect>
+    <!-- <h5>Remarks</h5> -->
     <div class="p-fluid formgrid grid">
+        <!-- <div class="field col-12 md:col-6">
+            <label >Service Rendered</label>
+            <Textarea v-model="value" rows="5" cols="30" />
+        </div> -->
         <div class="field col-12 md:col-6">
-            <label for="firstname2">Service Rendered</label>
+            <label >Tooth Number</label>
             <Textarea v-model="value" rows="5" cols="30" />
         </div>
         <div class="field col-12 md:col-6">
-            <label for="firstname2">Tooth Number</label>
+            <label >Medicine Given / Prescribed</label>
             <Textarea v-model="value" rows="5" cols="30" />
         </div>
-        <div class="field col-12 md:col-6">
-            <label for="firstname2">Medicine Given / Prescribed</label>
-            <Textarea v-model="value" rows="5" cols="30" />
-        </div>
-        <div class="field col-12 md:col-6">
-            <label for="firstname2">Remarks</label>
+        <div class="field col-12 md:col-12">
+            <label >Remarks</label>
             <Textarea v-model="value" rows="5" cols="30" />
         </div>
     </div>
