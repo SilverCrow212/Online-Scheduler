ad87b59eccfdaa261a9ab527a030441c688d9aa1
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-05 02:05:38 +0800

    fixed store for users

--- frontendSched/src/store/user.js
+++ frontendSched/src/store/user.js
@@ -2,9 +2,9 @@ import { defineStore } from 'pinia';
 import { Fragment, ref } from 'vue';
 
 export const user = defineStore('user', () => {
-    const userDetails = ref([
+    const userDetails = ref(
         {id: null, id_number:null, personal_details:null, status:null, created_at:null },
-    ]);
+    );
     return { userDetails };
 });
 
