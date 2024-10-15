e444446893ea7cee1cff2296e1d73c5bc2b25436
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-15 22:57:44 +0800

    fixed structure for getting user

--- frontendSched/src/store/user.js
+++ frontendSched/src/store/user.js
@@ -2,8 +2,12 @@ import { defineStore } from 'pinia';
 import { Fragment, ref } from 'vue';
 
 export const user = defineStore('user', () => {
-    const userDetails = ref(
-        {id: null, id_number:null, personal_details:null, status:null, created_at:null },
+    const userDetails = ref({
+        id: null, 
+        id_number:null, 
+        personal_details:null, 
+        status:null, 
+        created_at:null },
     );
     return { userDetails };
 });
