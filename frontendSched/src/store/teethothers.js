a296218b5d69c22798f15b617b4f190c116838d0
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-15 22:58:19 +0800

    added store for firstpage others and services rendered

---
+++ frontendSched/src/store/teethothers.js
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+import { Fragment, ref } from 'vue';
+
+export const otherInputs = defineStore('otherInputs', () => {
+    const firstPage= ref({
+        periodicalScreening:[],
+        occlusion:[],
+        appliances:[],
+        tmd:[],
+        tmdOthers:null,
+        others:null,
+        dateOfExamination1:null,
+        dateOfExamination2:null,
+        dateOfExamination3:null,
+        dateOfExamination4:null,
+        oralHygieneStatus1:null,
+        oralHygieneStatus2:null,
+        oralHygieneStatus3:null,
+        oralHygieneStatus4:null,
+    });
+
+    const servicesRendered = ref({
+        services:[],
+        toothnumber:null,
+        medicineprescribed:null,
+        remarks:null
+    });
+    return { firstPage, servicesRendered };
+});
