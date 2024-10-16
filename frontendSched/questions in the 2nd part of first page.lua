72bcbbfec7872aaf663319fc32ace4ae3bb0ba51
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-17 00:15:41 +0800

    added questions for 2nd part to be continued tomorrow

---
+++ frontendSched/questions in the 2nd part of first page.lua
@@ -0,0 +1,29 @@
+TREATMENT TO BE DONE: I understand and consent to have any treatment done by the dentist after the procedure, risks, benefits and costs have been fully explained. These treatments include, but not limited to, oral prophylaxis (scaling and polishing), tooth restoration (filling), tooth extraction, local anesthesia and other dental treatment that may arise during treatment or necessary for my oral health.
+Signature:
+
+DRUGS AND MEDICATIONS: I understand that antibiotics, analgesics, local anesthesia and other medications can cause allergic reactions like redness and swelling of tissues, pain, itching, vomiting and/or anaphylactic shock.
+Signature:
+
+CHANGES IN TREATMENT PLAN: I understand that during treatment, it may be necessary to change/add procedures because of conditions that may arise or was not discovered during examination while working on the tooth (for example, root canal therapy may be needed following routine restorative procedure). I give my permission to the dentist to make any/all changes and additions or he/she may refer me to private practitioners to do the necessary treatment.
+Signature:
+
+RADIOGRAPH: I understand that an x-ray shot or a radiograph maybe necessary as part of the diagnostic aid to come up with tentative diagnosis of my dental problem and to make a good treatment plan, but this will not give me a 100% assurance for the accuracy of the treatment since all dental treatments are subject to the unpredictable complications that later on may lead to sudden change of treatment and subject to new charges.
+Signature:
+
+REMOVAL OF TEETH: I understand there are alternatives to tooth removal (root canal therapy, crowns and periodontal surgery, etc.) and I completely understand these alternatives, inclding their risk and benefitss prior to authorizing the dentist to remove teeth and any other structures necessary for reasons above. I understand that removing teeth does not always remove all the infetions, if present, and it may be necessary to have further treatment. I understand the risk involved in having teeth removed, such as pain, swelling, sspread of infection, dry socket, fracture jaw, loss of feeling on the teeth, lips, tong, surrounding tissues that can last for an indefenite period of time. I understand that I may beed further treatment under a specialistif complications arises during or following treatment.
+Signature:
+
+PERIODONTAL DISEASE: I understand that periodontal disease is a serious condition causing gum and bone inflammation and/or loss and that can eventually lead to the loss of my teeth. I understand the alternative treatment plans to correct periodontal disease, including gum surgery, tooth extraction with or without replacement. I understand that undertaking any dental procedures may have future adverse effect on my periodontal conditions.
+Signature:
+
+FILLINGS: I understand that care must be exercised in chewing on fillings, especially during the first 24 hours to avoid breakage. I understand that an extensive filling or a crown may be required, as additional decay or fracture may become evident after initial excavation. I understand that siginifcant sensitivity is a common, but usually temporary, after-effect of a newly placed filling. I further understand that filling a tooth may irritate the nere tissue creating sensitivity and treating such sensitivity could require root canal therapy or extractions.
+Signature:
+
+
+
+I understand that dentistry is not an exact science that no dentist can properly guarantee accurate results all the time.
+I hereby authorize the dentist to proced with and perform dental treatment/s as explained to me. I understand that these are subject to modifications on undiagnosable circumstances that may arise during the course of treatment. The treatment were properly explained to me and any untoward circumstances that may arise during the procedure, the attending dentist will not be held liable since it is my free will, with full trust and confidence in him/her, to undergo dental treatment under his/her care.
+
+Patient Signature:
+Date:
+Dentist Signature:
