d6cfe085c0a8363df39809c1e507142aca11eecd
SilverCrow212 <135193347+SilverCrow212@users.noreply.github.com>
2024-10-17 23:12:31 +0800

    added store for account creation

---
+++ frontendSched/src/store/createacc.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+import { Fragment, ref } from 'vue';
+
+export const createacc = defineStore('createacc', () => {
+    const accDetails = ref({
+        school_id_number: null, 
+        password: null, 
+        firstname:null, 
+        lastname:null, 
+        middlename:null, 
+        type:null, 
+        employee_student_type:null, 
+        office_level:null, 
+        department_program:null, 
+        sex:null, 
+        email:null, 
+        contact_no:null, 
+        civil_status:null, 
+        guardian:null, 
+        guardian_no:null, 
+        permanent_address:null, 
+        bsu_address:null, 
+        updated_at:null, 
+        created_at:null },
+    );
+    return { accDetails };
+});
