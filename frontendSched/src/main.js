f9af4dc89da7de95e7ca1586d656eaf81af644cd
SilverCrow212 <dixonlexther21@gmail.com>
2024-10-20 02:58:49 +0800

    updated base url for axios

--- frontendSched/src/main.js
+++ frontendSched/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import AutoComplete from 'primevue/autocomplete';
