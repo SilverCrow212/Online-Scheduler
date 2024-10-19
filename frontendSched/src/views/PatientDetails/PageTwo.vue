2b59f365366496d4e9869a6b20d8ac09194a68a3
SilverCrow212 <dixonlexther21@gmail.com>
2024-10-20 03:02:07 +0800

    updated the store in page 2

--- frontendSched/src/views/PatientDetails/PageTwo.vue
+++ frontendSched/src/views/PatientDetails/PageTwo.vue
@@ -1,15 +1,19 @@
 <script setup>
 import {ref} from 'vue'
 import {serviceRendered} from '@/store/choices'
+import {otherInputs} from '@/store/teethothers';
 const serviceRenderedStore = serviceRendered();
 const serviceChoices = serviceRenderedStore.legend;
+const otherInputsStore = otherInputs();
 
+const otherInput = otherInputsStore.servicesRendered;
 const multiselectValue = ref(null);
 const value = ref();
 </script>
 <template>
+    {{ otherInputsStore }}
     <h5>Services Rendered</h5>
-    <MultiSelect v-model="multiselectValue" :options="serviceChoices" optionLabel="name" placeholder="Select Services" :filter="true" class="w-full">
+    <MultiSelect v-model="otherInput.services" :options="serviceChoices" optionLabel="name" placeholder="Select Services" :filter="true" class="w-full">
         <template #value="slotProps">
             <div class="inline-flex align-items-center py-1 px-2 bg-primary text-primary border-round mr-2" v-for="option in slotProps.value" :key="option.id">
                 <div>{{ option.name }}</div>
@@ -32,15 +36,15 @@ const value = ref();
         </div> -->
         <div class="field col-12 md:col-6">
             <label >Tooth Number</label>
-            <Textarea v-model="value" rows="5" cols="30" />
+            <Textarea v-model="otherInput.toothnumber" rows="5" cols="30" />
         </div>
         <div class="field col-12 md:col-6">
             <label >Medicine Given / Prescribed</label>
-            <Textarea v-model="value" rows="5" cols="30" />
+            <Textarea v-model="otherInput.medicineprescribed" rows="5" cols="30" />
         </div>
         <div class="field col-12 md:col-12">
             <label >Remarks</label>
-            <Textarea v-model="value" rows="5" cols="30" />
+            <Textarea v-model="otherInput.remarks" rows="5" cols="30" />
         </div>
     </div>
 </template>
