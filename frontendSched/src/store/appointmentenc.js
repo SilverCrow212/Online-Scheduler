fc54711409ba230253d25367389619b068c47395
SilverCrow212 <dixonlexther21@gmail.com>
2024-10-27 01:59:17 +0800

    fixed store for clinical details

--- frontendSched/src/store/appointmentenc.js
+++ frontendSched/src/store/appointmentenc.js
@@ -3,15 +3,31 @@ import { Fragment, ref } from 'vue';
 
 export const appointment = defineStore('appointment', () => {
     const userDetails = ref([
-        {id: null, 
-        user_details_id:null,
-        consent_form:null,
-        enccode:null, 
-        appointment_date:null, 
-        appointment_time:null, 
-        // clinicalDetails:null, 
-        status:null,
-        created_at:null },
+        {
+            id: null, 
+            user_details_id:null,
+            consent_form:null,
+            enccode:null, 
+            appointment_date:null, 
+            appointment_time:null, 
+            status:null,
+            created_at:null,
+            updated_at:null,
+     },
     ]);
-    return { userDetails };
+
+    const clinicalDetails = ref([
+        {
+            id: null, 
+            appointment_id:null,
+            hygiene_status:null,
+            services_rendered:null, 
+            teeth_questionmark:null, 
+            medicine_given:null, 
+            remarks:null,
+            created_at:null,
+            updated_at:null,
+        },
+    ]);
+    return { userDetails, clinicalDetails };
 });
