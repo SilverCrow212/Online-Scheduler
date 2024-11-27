efa41fa2fced3415fc0e783e214e35337b05b9bf
Jiison <54047528+jaelzki24@users.noreply.github.com>
2024-11-27 15:34:04 +0800

    - - - test

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Spatie\Backup\Events\BackupHasFailed;
+use Illuminate\Support\Facades\Event;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Event::forget(BackupHasFailed::class);
     }
 }
